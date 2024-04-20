114c9a713b774984e0f404e92f287e5576eb7df9
PedroScheffer <pscheffergoncalves@gmail.com>
2024-04-20 08:27:24 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿while (true)
+{
+    Console.WriteLine("Digite a sua altura (em centimetros) ou 'sair' para encerrar o programa: ");
+    string input = Console.ReadLine();
+    
+    if (input.ToLower() == "sair")
+    break;
+
+    double altura;
+
+    if (Double.TryParse(input, out altura)) 
+    {
+        altura = altura /100;
+    }
+
+    else 
+    {
+        Console.WriteLine("Digite um número válido!");
+    }
+    
+    Console.WriteLine("Digite o seu peso: ");
+    double peso = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Digite a sua idade: ");
+    int idade = Convert.ToInt16(Console.ReadLine());
+    Console.WriteLine("Digite o seu nome: ");
+    var nome = Console.ReadLine();
+   
+    double imc = peso / (altura * altura);
+
+    if (imc < 18.5)
+    {
+        Console.WriteLine($"{nome} | {idade} anos, está abaixo do peso ideal!");
+    }
+    else if (imc >= 18.5 && imc < 24.9)
+    {
+        Console.WriteLine($"{nome} | {idade} anos, está no peso ideal!");
+    }
+    else if (imc >= 25.0 && imc < 29.9)
+    {
+        Console.WriteLine($"{nome} | {idade} anos, está acima do peso ideal!");
+    }
+
+    else
+    {
+        Console.WriteLine($"{nome} | {idade} anos, está obeso!");
+    }
+
+}
